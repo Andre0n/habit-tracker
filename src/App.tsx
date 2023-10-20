36be939b7662dacca3b7a92919ece33c59afeb49
Andre0n <andreon098@gmail.com>
2023-10-20 13:36:04 -0300

    feat: Add home screen basic structure
    - Create the main components and layout for the home screen
    - Include placeholder content and styling
    This commit introduces the basic structure for the home screen,
    including its main components, layout, placeholder content, and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 const App = () => {
-  return <h1> Habit App </h1>;
+  return (
+    <div className="flex flex-col gap-4 p-4 h-screen justify-between">
+      <header>
+        <h1 className="text-xl font-medium">Habits</h1>
+      </header>
+      <main className="flex flex-col flex-1">
+        <section className="flex flex-col h-1/2 gap-4">
+          <h2 className="text-md font-medium">In Progress Habit:</h2>
+          <span className="text-sm text-center"> No habits in progress</span>
+        </section>
+        <section className="flex flex-col h-1/2 gap-4">
+          <h2 className="text-md font-medium">Completed Habit:</h2>
+          <span className="text-sm text-center">No completed habits</span>
+        </section>
+      </main>
+      <footer>
+        <nav className="flex justify-between">
+          <button>Home</button>
+          <button>Add</button>
+          <button>Statitics</button>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default App;
