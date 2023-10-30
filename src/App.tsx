2a19e8ae8e33e90ac2218b9292b410a46a79a445
Andre0n <andreon098@gmail.com>
2023-10-30 12:16:23 -0300

    fix(App.tsx): Sort the classes correctly
    Description:
    - Update code to ensure classes are sorted in the correct order
    - Improve readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 const App = () => {
   return (
-    <div className="flex flex-col gap-4 p-4 h-screen justify-between">
+    <div className="flex h-screen flex-col justify-between gap-4 p-4">
       <header>
         <h1 className="text-xl font-medium">Habits</h1>
       </header>
-      <main className="flex flex-col flex-1">
-        <section className="flex flex-col h-1/2 gap-4">
+      <main className="flex flex-1 flex-col">
+        <section className="flex h-1/2 flex-col gap-4">
           <h2 className="text-md font-medium">In Progress Habit:</h2>
-          <span className="text-sm text-center"> No habits in progress</span>
+          <span className="text-center text-sm"> No habits in progress</span>
         </section>
-        <section className="flex flex-col h-1/2 gap-4">
+        <section className="flex h-1/2 flex-col gap-4">
           <h2 className="text-md font-medium">Completed Habit:</h2>
-          <span className="text-sm text-center">No completed habits</span>
+          <span className="text-center text-sm">No completed habits</span>
         </section>
       </main>
       <footer>
